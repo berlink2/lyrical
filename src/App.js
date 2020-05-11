5f1c5d29939472c16455e8dfa92ace1b4f7c1692
berlink2 <berlink.k2@gmail.com>
2020-05-11 14:49:58 +0700

    hash

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Navbar} from './components/layout/navbar';
 import {Index} from './components/layout/index'
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import {HashRouter as Router,Route,Switch} from 'react-router-dom';
 import {Provider} from './context'
 import {Lyrics} from './components/songs/Lyrics'
 
