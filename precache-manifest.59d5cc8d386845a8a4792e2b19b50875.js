67a66d50d392df8f415f0bfe400412076f2b2769
berlink2 <berlink.k2@gmail.com>
2020-05-11 11:19:42 +0700

    Updates

---
+++ precache-manifest.59d5cc8d386845a8a4792e2b19b50875.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de9f1e0b0ac3c622c5d54cc2e9690198",
+    "url": "/lyrical/index.html"
+  },
+  {
+    "revision": "9e68d1e8fb398796f9b1",
+    "url": "/lyrical/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "627241b8d7a446211ab7",
+    "url": "/lyrical/static/js/2.0e64ffa9.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/lyrical/static/js/2.0e64ffa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e68d1e8fb398796f9b1",
+    "url": "/lyrical/static/js/main.21dad98e.chunk.js"
+  },
+  {
+    "revision": "b46f0e17a16f93d33c5c",
+    "url": "/lyrical/static/js/runtime-main.c40f41d9.js"
+  },
+  {
+    "revision": "ffc52f256266d44463482fe0a3c319a0",
+    "url": "/lyrical/static/media/spinner.ffc52f25.gif"
+  }
+]);
